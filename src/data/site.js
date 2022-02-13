8ca6a4af4e7e976d99aa61fe679c6ca8f6d36d47
Richard Miller <richardmmill@gmail.com>
2022-02-13 15:05:59 -0500

    site.js for BaseHead data

---
+++ src/data/site.js
@@ -0,0 +1,5 @@
+export default {
+    title: `Richard the Dev`,
+    description: `Richard is a Cincinnati based Software Engineer/Web Developer with a passion for frontend and accessibility.`,
+    url: `https://www.richardmillerwimmer.dev/`,
+}
