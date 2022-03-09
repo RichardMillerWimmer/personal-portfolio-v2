73ee08bcf65cba86173e30bdf725e221053d8d66
Richard Miller <richardmmill@gmail.com>
2022-03-09 10:15:37 -0500

    start of work and getStaticPaths

--- astro/src/components/Nav/NavList.tsx
+++ astro/src/components/Nav/NavList.tsx
@@ -5,6 +5,37 @@ interface NavProps {
     setOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
 
+type StyledUIType = {
+    open: boolean
+}
+
+const Ul = styled.ul<StyledUIType>`
+z-index: 1;
+list-style: none;
+display: flex;
+flex-flow: row nowrap;
+justify-content: center;
+li {
+    text-align: center;
+    padding: 18px 10px;
+}
+@media (max-width: 768px) {
+    flex-flow: column nowrap;
+    background-color: #3d3d3d;
+    position: fixed;
+    transform: ${({ open }) => open ? 'translateY(0)' : 'translateY(100%)'};
+    top: 0;
+    right: 0;
+    height: 100vh;
+    width: 100%;
+    padding-top: 3.5rem;
+    transition: transform 0.3s ease-in-out;
+    li {
+        color: #fff;
+    }    
+}
+`
+
 const NavList: React.FC = ({ open, setOpen }: NavProps): any => {
     const router = useRouter();
 
