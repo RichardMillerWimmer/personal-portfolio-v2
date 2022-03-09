ac5ec5edd794562c0f7656e2aa0fb9681e69db44
Richard Miller <richardmmill@gmail.com>
2022-03-09 10:04:40 -0500

    more nav in place

--- astro/src/components/Nav/Burger.tsx
+++ astro/src/components/Nav/Burger.tsx
@@ -2,9 +2,44 @@ import { useState } from 'react';
 import NavList from './NavList';
 
 
-// type StyledBurgerType = {
-//   open: boolean
-// }
+type StyledBurgerType = {
+    open: boolean
+}
+
+const StyledBurger = styled.div<StyledBurgerType>`
+  width: 2rem;
+  height: 2rem;
+  position: fixed;
+  bottom: 12px;
+  left: 50%;
+  transition: all 0.3s linear;
+  transform: ${({ open }) => open ? 'translate(-35%, 0)' : 'translate(-46%)'};
+  z-index: 20;
+  display: none;
+  @media (max-width: 768px) {
+    display: flex;
+  justify-content: space-around;
+  flex-flow: column nowrap;
+}
+  div {
+    width: 2rem;
+  height: 0.25rem;
+  background-color: #fff;
+  border-radius: 10px;
+  transform-origin: 1px;
+  transition: all 0.3s linear;
+  &:nth-child(1) {
+    transform: ${({ open }) => open ? 'rotate(45deg) scale(1.05, 1)' : 'rotate(0)'};
+  }
+  &:nth-child(2) {
+    transform: ${({ open }) => open ? 'translateX(100%)' : 'translateX(0)'};
+  opacity: ${({ open }) => open ? 0 : 1};
+  }
+  &:nth-child(3) {
+    transform: ${({ open }) => open ? 'rotate(-45deg) scale(1.05, 1)' : 'rotate(0)'};
+  }
+}
+  `
 
 const Burger: React.FC = () => {
     const [open, setOpen] = useState<boolean>(false)
--- astro/src/components/Nav/NavList.tsx
+++ astro/src/components/Nav/NavList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useRouter, Link } from 'react-router'
+import { useRouter, Link } from 'react-router-dom'
 interface NavProps {
     open: boolean,
     setOpen: React.Dispatch<React.SetStateAction<boolean>>
@@ -7,16 +7,14 @@ interface NavProps {
 
 const NavList: React.FC = ({ open, setOpen }: NavProps): any => {
     const router = useRouter();
-    // console.log(router)
 
     return (
-        // <ul open={open}>
-        //     <li className={router.pathname === "/" ? styles.active : ""} onClick={() => setOpen(!open)}><Link href='/' ><a>Home</a></Link></li>
-        //     <li className={router.pathname === "/projects" ? styles.active : ""} onClick={() => setOpen(!open)}><Link href='/projects'><a>Projects</a></Link></li>
-        //     <li className={router.pathname === "/about" ? styles.active : ""} onClick={() => setOpen(!open)}><Link href='/about'><a>About</a></Link></li>
-        //     <li className={router.pathname === "/cv" ? styles.active : ""} onClick={() => setOpen(!open)}><Link href='/cv'><a>CV</a></Link></li>
-        //     <li className={router.pathname === "/contact" ? styles.active : ""} onClick={() => setOpen(!open)}><Link href='/contact'><a>Contact</a></Link></li>
-        // </ul>
+        <ul open={open}>
+            <li className={router.pathname === "/" ? styles.active : ""} onClick={() => setOpen(!open)}><Link to='/' >Home</Link></li>
+            <li className={router.pathname === "/work" ? styles.active : ""} onClick={() => setOpen(!open)}><Link to='/work'><a>Work</a></Link></li>
+            <li className={router.pathname === "/about" ? styles.active : ""} onClick={() => setOpen(!open)}><Link to='/about'>About</Link></li>
+            <li className={router.pathname === "/contact" ? styles.active : ""} onClick={() => setOpen(!open)}><Link to='/contact'>Contact</Link></li>
+        </ul>
     )
 }
 
