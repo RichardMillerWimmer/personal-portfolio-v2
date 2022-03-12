daece15666c8521fd9f70dd431d0c99b99a7d3bd
Richard Miller <richardmmill@gmail.com>
2022-03-12 10:01:14 -0500

    work on nav styling

--- astro/src/components/Nav/NavLink.tsx
+++ astro/src/components/Nav/NavLink.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../../styles/global.scss'
+import styles from '../../styles/NavLink.module.scss'
 
 type NavLinkType = {
     text: string
@@ -13,7 +13,7 @@ const NavLink = ({ text, href, page }: NavLinkType): JSX.Element => {
     }
 
     return (
-        <li><a className={`${page === `/${text}` ? 'is-active' : ''}`} href={href}>{text}</a></li>)
+        <li><a className={`${page === `/${text}` ? { styles.isActive } : ''}`} href={href}>{text}</a></li>)
 }
 
 export default NavLink
