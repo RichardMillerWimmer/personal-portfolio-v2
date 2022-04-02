4c56dff333ebdbbe55cadea52324d2d1a1da3333
Richard Miller <richardmmill@gmail.com>
2022-04-01 20:24:35 -0400

    v0.25 migragtion start

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,4 +1,7 @@
-export default {
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+
+export default defineConfig({
   // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
   // pages: './src/pages', // Path to Astro components, pages, and data
   // dist: './dist',       // When running `astro build`, path to final static output
@@ -11,7 +14,5 @@ export default {
     // hostname: 'localhost',  // The hostname to run the dev server on.
     // port: 3000,             // The port to run the dev server on.
   },
-  renderers: [
-    "@astrojs/renderer-react"
-  ],
-};
+  integrations: [react()]
+});
