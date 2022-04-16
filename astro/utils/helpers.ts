9444ae0079e3c123fac00d29af0afe2703ad692a
Richard Miller <richardmmill@gmail.com>
2022-04-16 16:59:20 -0400

    helpers util for blog

---
+++ astro/utils/helpers.ts
@@ -0,0 +1,26 @@
+type slugObj = {
+    name: string;
+    slug: string;
+}
+
+function generateSlug(category: string): string {
+    return category
+    .toString()
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w\-]+/g, '')
+    .replace(/\-\-+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '');
+}
+
+export function generateCategoriesData(categories: string[]): slugObj[] {
+    let categoriesData = categories.map((elem) => {
+        return {
+            name: elem,
+            slug: `${generateSlug(elem)}` 
+        }
+    })
+    return categoriesData
+}
