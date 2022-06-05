cd542423eca11967d85743fe89a1221eb792e5da
Richard Miller <richardmmill@gmail.com>
2022-06-05 18:22:40 -0400

    dynamic img arrry

--- astro/src/components/Carousel.tsx
+++ astro/src/components/Carousel.tsx
@@ -4,12 +4,14 @@ import Img2 from '/assets/work/TopTableGamesThumbnail.png'
 import Img3 from '/assets/work/ValeriePortfolioThumbnail.png'
 import styles from '../styles/Carousel.module.scss'
 
-const Carousel = ({ imgArray }) => {
+const testArr = [Img1, Img2, Img3]
+
+const Carousel = ({ }) => {
     const [active, setActive] = useState(0)
-    const length = imgArray.length
+    // const length = imgArray.length
 
     const nextSlide = () => {
-        
+
     }
     const prevSlide = () => {
 
@@ -18,22 +20,12 @@ const Carousel = ({ imgArray }) => {
     return (
         <div>
             <div className={styles.slideshowContainer}>
-
-                <div className={styles.slides}>
-                    <div className={styles.numberText}>1 / 3</div>
-                    <img className={styles.slideImg} src={Img1} />
-                </div>
-
-                <div className={styles.slides}>
-                    <div className={styles.numberText}>2 / 3</div>
-                    <img className={styles.slideImg} src={Img2} />
-                </div>
-
-                <div className={styles.slides}>
-                    <div className={styles.numberText}>3 / 3</div>
-                    <img className={styles.slideImg} src={Img3} />
-                </div>
-
+                {testArr.map((img) => {
+                    return <div className={styles.slides}>
+                        <div className={styles.numberText}>1 / 3</div>
+                        <img className={styles.slideImg} src={img} />
+                    </div>
+                })}
                 <a className={styles.prev} onClick={() => { }}>&#10094;</a>
                 <a className={styles.next} onClick={() => { }}>&#10095;</a>
             </div>
