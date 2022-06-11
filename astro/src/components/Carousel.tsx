300c2942c6cba13adcee62b3089b6141193a9fc0
Richard Miller <richardmmill@gmail.com>
2022-06-10 20:11:53 -0400

    work thumbbail style fix

--- astro/src/components/Carousel.tsx
+++ astro/src/components/Carousel.tsx
@@ -7,31 +7,31 @@ import styles from '../styles/Carousel.module.scss'
 const testArr = [Img1, Img2, Img3]
 
 const Carousel = () => {
-    const [active, setActive] = useState<number>(0)
+    const [current, setCurrent] = useState<number>(0)
     const length = testArr.length
-    console.log(active)
-    console.log(testArr[active])
+    console.log(current)
+    console.log(testArr[current])
 
 
     const renderDotSet = (length) => {
         let dots = []
         for (let i = 0; i < length; i++) {
-            dots.push(<span key={i} className={styles.dot} onClick={() => { setActive(i) }}></span>)
+            dots.push(<span key={i} className={styles.dot} onClick={() => { setCurrent(i) }}></span>)
         }
         return dots
     }
 
     const nextSlide = () => {
-        setActive(active === testArr.length - 1 ? 0 : active + 1)
+        setCurrent(current === testArr.length - 1 ? 0 : current + 1)
     }
     const prevSlide = () => {
-        setActive(active === 0 ? testArr.length - 1 : active - 1)
+        setCurrent(current === 0 ? testArr.length - 1 : current - 1)
     }
 
     const renderSlide = testArr.map((img, index) => {
         return (
-            <div className={index === active ? styles.slides : styles.active} key={index}>
-                <div className={styles.numberText}>{active + 1} / {length}</div>
+            <div className={index === current ? styles.slides : styles.active} key={index}>
+                <div className={styles.numberText}>{current + 1} / {length}</div>
                 <img className={styles.slideImg} src={img} />
             </div>
         )
