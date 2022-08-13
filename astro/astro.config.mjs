89e0331147d7e0f94bf2a93113c517b441df722c
Richard Miller <richardmmill@gmail.com>
2022-08-13 08:25:08 -0400

    markdown migration

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap'
+import mdx from '@astrojs/mdx'
 
 export default defineConfig({
     // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
@@ -8,5 +9,5 @@ export default defineConfig({
     // dist: './dist',       // When running `astro build`, path to final static output
     // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
     site: 'http://richardthe.dev',
-    integrations: [sitemap(), react()]
+    integrations: [sitemap(), react(), mdx()]
 });
