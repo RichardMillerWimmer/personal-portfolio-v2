ef248bfba008dedc8373a16af5689934c00497cd
Richard Miller <richardmmill@gmail.com>
2022-10-11 18:56:16 -0400

    blogcard underline

--- astro/src/components/Nav/IconLink.tsx
+++ astro/src/components/Nav/IconLink.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from '../../styles/IconLink.module.scss'
+import WorkIcon from './NavIcons/WorkIcon.astro'
 
 type IconLinkType = {
     text: string;
@@ -8,13 +9,13 @@ type IconLinkType = {
     child: JSX.Element
 }
 
-const NavLink = ({ text, href, page }: IconLinkType): JSX.Element => {
+const NavLink = ({ text, href, page, child }: IconLinkType): JSX.Element => {
     if (page === '/') {
         page = '/home'
     }
 
     return (
-        <li><a className={`${page === `/${text}` ? `${styles.isActive} ${styles.navLink}` : styles.navLink}`} href={href}>{text}</a></li >)
+        <li><a className={`${page === `/${text}` ? `${styles.isActive} ${styles.navLink}` : styles.navLink}`} href={href}><WorkIcon /></a></li >)
 }
 
 export default NavLink
