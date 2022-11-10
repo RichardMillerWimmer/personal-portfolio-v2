98bcc0f99df78f9823b4d99996bb7100fb25f45a
Richard Miller <richardmmill@gmail.com>
2022-11-10 08:03:28 -0500

    Nalist to react tsx

---
+++ astro/src/components/Nav/NavList.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import NavLink from './NavLink.tsx'
+import styles from '../../styles/NavList.module.scss'
+
+
+const NavList = () => {
+    const page = window.location.pathname
+    return (
+        <nav>
+            <ul className={styles.navList}>
+                <NavLink page={page} text='home' href='/' />
+                <NavLink page={page} text='work' href='/work' />
+                <NavLink page={page} text='about' href='/about' />
+                <NavLink page={page} text='blog' href='/blog' />
+                <NavLink page={page} text='contact' href='/contact' />
+            </ul>
+        </nav>
+    )
+}
+
+export default NavList
+
