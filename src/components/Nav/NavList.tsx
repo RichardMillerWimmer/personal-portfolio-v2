29f624431e655a3b5ca901d626b9ef61c15d07fe
Richard Miller Wimmer <78984588+RichardMillerWimmer@users.noreply.github.com>
2022-11-26 08:28:21 -0500

    Feature Remove modules in Nav styling (#2)
    * remove modules in nav

--- src/components/Nav/NavList.tsx
+++ src/components/Nav/NavList.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import NavLink from './NavLink.tsx'
-import styles from '../../styles/NavList.module.scss'
+// import styles from '../../styles/NavList.module.scss'
+import '../../styles/NavList.scss'
 
 
 const NavList = () => {
     const page = window.location.pathname
     return (
         <nav>
-            <ul className={styles.navList}>
+            <ul className='navList'>
                 <NavLink page={page} text='home' href='/' />
                 <NavLink page={page} text='work' href='/work' />
                 <NavLink page={page} text='about' href='/about' />
