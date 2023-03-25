4130be7716da58b41934a889a223514ee8a08743
Richard Miller <richardmmill@gmail.com>
2023-03-25 14:39:54 -0400

    consolidate links

--- src/components/Nav/NavLink.tsx
+++ src/components/Nav/NavLink.tsx
@@ -6,19 +6,28 @@ type NavLinkType = {
     text: string;
     href: string;
     page: string;
+    child?: string
 }
 
-const NavLink = ({ text, href, page }: NavLinkType): JSX.Element => {
+const NavLink = ({ text, href, page, child }: NavLinkType): JSX.Element => {
     if (page === '/') {
         page = '/home'
+    }
+
+    const renderLink = () => {
+        return child ? <img src={child} alt={text} /> : text
     }
 
     const regex = new RegExp(`/${text}`, 'g')
     const isCurrentPage = regex.test(page)
 
+
+
     return (
         <li className={styles.navWrapper}>
-            <a aria-current={isCurrentPage ? 'page' : null} className={isCurrentPage ? `${styles.isActive} ${styles.navLink}` : `${styles.navLink}`} href={href}>{text}</a>
+            <a aria-current={isCurrentPage ? 'page' : null} className={isCurrentPage && !child ? `${styles.isActive} ${styles.navLink}` : `${styles.navLink}`} href={href}>
+                {renderLink()}
+            </a>
         </li >)
 }
 
