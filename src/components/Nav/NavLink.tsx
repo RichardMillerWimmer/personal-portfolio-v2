de48c3d4208bedc5d35144172452c8e9ebeb75c6
Richard Miller Wimmer <78984588+RichardMillerWimmer@users.noreply.github.com>
2023-03-25 15:13:39 -0400

    Feature consolidate links (#10)
    * consolidate links
    
    * implement NavLink in BlogNav
    
    * remove iconlink
    
    * consolidate conditional styles
    
    * remove uneeded css

--- src/components/Nav/NavLink.tsx
+++ src/components/Nav/NavLink.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import styles from '../../styles/NavLink.module.scss'
-// import '../../styles/NavLink.css'
 
 type NavLinkType = {
     text: string;
     href: string;
     page: string;
+    child?: string
 }
 
-const NavLink = ({ text, href, page }: NavLinkType): JSX.Element => {
+const NavLink = ({ text, href, page, child }: NavLinkType): JSX.Element => {
     if (page === '/') {
         page = '/home'
     }
@@ -16,9 +16,17 @@ const NavLink = ({ text, href, page }: NavLinkType): JSX.Element => {
     const regex = new RegExp(`/${text}`, 'g')
     const isCurrentPage = regex.test(page)
 
+    const renderLink = () => {
+        return child ? <img src={child} alt={text} /> : text
+    }
+
+    const navClasses = !child ? isCurrentPage ? `${styles.isActive} ${styles.navLink} ${styles.bars}` : `${styles.navLink} ${styles.bars}` : `${styles.navLink}`
+
     return (
         <li className={styles.navWrapper}>
-            <a aria-current={isCurrentPage ? 'page' : null} className={isCurrentPage ? `${styles.isActive} ${styles.navLink}` : `${styles.navLink}`} href={href}>{text}</a>
+            <a aria-current={isCurrentPage ? 'page' : null} className={navClasses} href={href}>
+                {renderLink()}
+            </a>
         </li >)
 }
 
