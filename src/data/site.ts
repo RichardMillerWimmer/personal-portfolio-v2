c5eece74f2f8ea0faf2554d4cb0b576ee02edbb5
Richard Miller <richardmmill@gmail.com>
2024-03-09 13:12:27 -0500

    update meta titles and descriptions

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,5 +1,5 @@
 export default {
     title: `Richard the Dev`,
-    description: `Richard Miller Wimmer is a Cincinnati based Software Engineer/Web Developer with a passion for frontend and accessibility.`,
+    description: `Richard Miller Wimmer is a Cincinnati based Software Engineer/Web Developer with a passion for frontend development and accessibility.`,
     url: `https://www.richardthe.dev/`,
 }
